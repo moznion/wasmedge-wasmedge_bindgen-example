0f6cd2d95e8a000fb41d5e6145b6409a0344841d
moznion <moznion@mail.moznion.net>
2023-10-24 14:50:53 -0700

    Return result
    Signed-off-by: moznion <moznion@mail.moznion.net>

--- wasm-app/src/lib.rs
+++ wasm-app/src/lib.rs
@@ -3,6 +3,7 @@ use wasmedge_bindgen::*;
 use wasmedge_bindgen_macro::*;
 
 #[wasmedge_bindgen]
-pub fn run(args: Vec<u8>) {
+pub fn run(args: Vec<u8>) -> Result<(), String> {
     println!("{:?}", args);
+    Ok(())
 }
